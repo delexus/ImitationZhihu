3f213adf8dc8336da5bb57b6edb643d39773778e
delexus <delexus@163.com>
2017-03-13 18:01:39 +0800

    更新底部导航按钮

--- app/src/main/java/com/delexus/imitationzhihu/MainActivity.java
+++ app/src/main/java/com/delexus/imitationzhihu/MainActivity.java
@@ -1,18 +1,56 @@
 package com.delexus.imitationzhihu;
 
+import android.content.res.ColorStateList;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by ly-yangyuzhi on 2017/3/3.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private RadioImageButton mBtnFeed, mBtnDiscover, mBtnNotify,
+                        mBtnMessage, mBtnMore;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        mBtnFeed = (RadioImageButton) findViewById(R.id.btn_feed);
+        mBtnDiscover = (RadioImageButton) findViewById(R.id.btn_discover);
+        mBtnNotify = (RadioImageButton) findViewById(R.id.btn_notification);
+        mBtnMessage = (RadioImageButton) findViewById(R.id.btn_message);
+        mBtnMore = (RadioImageButton) findViewById(R.id.btn_more);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_feed:
+
+
+                break;
+            case R.id.btn_discover:
+
+                break;
+            case R.id.btn_notification:
+
+                break;
+            case R.id.btn_message:
+
+                break;
+            case R.id.btn_more:
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/delexus/imitationzhihu/RadioImageButton.java
@@ -0,0 +1,106 @@
+package com.delexus.imitationzhihu;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.CompoundButton;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by ly-yangyuzhi on 2017/3/13.
+ */
+
+public class RadioImageButton extends CompoundButton {
+
+    private Drawable mDrawable;
+
+    public RadioImageButton(Context context) {
+        super(context);
+    }
+
+    public RadioImageButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mDrawable = (Drawable) reflectParentFieldAndReturn(this, getClass().getSuperclass().getName(),
+                "mButtonDrawable");
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        final Drawable buttonDrawable = mDrawable;
+        if (buttonDrawable != null) {
+            final int verticalGravity = getGravity() & Gravity.VERTICAL_GRAVITY_MASK;
+            final int drawableHeight = buttonDrawable.getIntrinsicHeight();
+            final int drawableWidth = buttonDrawable.getIntrinsicWidth();
+
+            final int top;
+            switch (verticalGravity) {
+                case Gravity.BOTTOM:
+                    top = getHeight() - drawableHeight;
+                    break;
+                case Gravity.CENTER_VERTICAL:
+                    top = (getHeight() - drawableHeight) / 2;
+                    break;
+                default:
+                    top = 0;
+            }
+            final int bottom = top + drawableHeight;
+            final int horizontalGravity = getGravity() & Gravity.HORIZONTAL_GRAVITY_MASK;
+            final int left;
+            switch (horizontalGravity) {
+                case Gravity.CENTER_HORIZONTAL:
+                    left = (getWidth() - drawableWidth) / 2;
+                    break;
+                default:
+                    left = 0;
+                    break;
+            }
+            final int right = left + drawableWidth;
+            buttonDrawable.setBounds(left, top, right, bottom);
+
+            final Drawable background = getBackground();
+            if (background != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                background.setHotspotBounds(left, top, right, bottom);
+            }
+
+            buttonDrawable.draw(canvas);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * If the radio button is already checked, this method will not toggle the radio button.
+     */
+    @Override
+    public void toggle() {
+        // we override to prevent toggle when the radio is already
+        // checked (as opposed to check boxes widgets)
+        if (!isChecked()) {
+            super.toggle();
+        }
+    }
+
+    public static Object reflectParentFieldAndReturn(Object object, String clazzName, String fieldName) {
+        Field field = null;
+        Object ret = null;
+        try {
+            for (Class clazz = object.getClass(); clazz != Object.class; clazz = clazz.getSuperclass()) {
+                if (clazz.getName().equals(clazzName))
+                    field = clazz.getDeclaredField(fieldName);
+            }
+            if (field != null) {
+                field.setAccessible(true);
+                ret = field.get(object);
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+}
