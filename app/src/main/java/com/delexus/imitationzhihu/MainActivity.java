27043c54e92be2993f53f021e81aa4315f9f52f2
delexus <delexus@163.com>
2017-03-15 18:10:28 +0800

    滑动交互

--- app/src/main/java/com/delexus/imitationzhihu/MainActivity.java
+++ app/src/main/java/com/delexus/imitationzhihu/MainActivity.java
@@ -1,12 +1,17 @@
 package com.delexus.imitationzhihu;
 
-import android.content.res.ColorStateList;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by ly-yangyuzhi on 2017/3/3.
@@ -16,12 +21,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private RadioImageButton mBtnFeed, mBtnDiscover, mBtnNotify,
                         mBtnMessage, mBtnMore;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        loadData();
     }
 
     private void init() {
@@ -30,6 +37,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtnNotify = (RadioImageButton) findViewById(R.id.btn_notification);
         mBtnMessage = (RadioImageButton) findViewById(R.id.btn_message);
         mBtnMore = (RadioImageButton) findViewById(R.id.btn_more);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    }
+
+    private void loadData() {
+        SimpleAdapter adapter = new SimpleAdapter();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -51,6 +66,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_more:
 
                 break;
+        }
+    }
+
+    private static class SimpleAdapter extends RecyclerView.Adapter {
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MyHolder(new TextView(parent.getContext()));
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            MyHolder myHolder = (MyHolder) holder;
+            myHolder.mTextView.setText(String.valueOf(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 60;
+        }
+    }
+
+    private static class MyHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+
+        public MyHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView;
         }
     }
 }
---
+++ app/src/main/java/com/delexus/imitationzhihu/MyCoordinatorLayout.java
@@ -0,0 +1,25 @@
+package com.delexus.imitationzhihu;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by ly-yangyuzhi on 2017/3/14.
+ */
+
+public class MyCoordinatorLayout extends CoordinatorLayout {
+    public MyCoordinatorLayout(Context context) {
+        super(context);
+    }
+
+    public MyCoordinatorLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyCoordinatorLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
