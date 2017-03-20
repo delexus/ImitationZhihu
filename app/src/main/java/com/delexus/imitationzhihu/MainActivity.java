205b73ffbc9772181c7e457010770b0ede13e5ef
delexus <delexus@163.com>
2017-03-20 11:58:10 +0800

    回滚到滑动和bottombar交互

--- app/src/main/java/com/delexus/imitationzhihu/MainActivity.java
+++ app/src/main/java/com/delexus/imitationzhihu/MainActivity.java
@@ -1,27 +1,35 @@
 package com.delexus.imitationzhihu;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 /**
  * Created by ly-yangyuzhi on 2017/3/3.
  */
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private RadioImageButton mBtnFeed, mBtnDiscover, mBtnNotify,
-                        mBtnMessage, mBtnMore;
+            mBtnMessage, mBtnMore;
     private RecyclerView mRecyclerView;
+
+    private CoordinatorLayout mCoordinatorLayout;
+    private RadioImageGroup mRadioImageGroup;
+    private FloatingActionButton mFab;
+
+    private boolean mIsBottomVisible = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,7 +46,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtnMessage = (RadioImageButton) findViewById(R.id.btn_message);
         mBtnMore = (RadioImageButton) findViewById(R.id.btn_more);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+        mRadioImageGroup = (RadioImageGroup) findViewById(R.id.main_group);
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                Log.d(TAG, "dx = " + String.valueOf(dx) + "dy = " + String.valueOf(dy));
+                if (dy > 0 && mIsBottomVisible) { // 不可见
+                    mIsBottomVisible = false;
+                    ObjectAnimator animator = ObjectAnimator.ofFloat(mRadioImageGroup, View.TRANSLATION_Y,
+                            mRadioImageGroup.getTranslationY(), mRadioImageGroup.getTranslationY() + mRadioImageGroup.getHeight());
+                    ObjectAnimator fabAnimator = ObjectAnimator.ofFloat(mFab, View.TRANSLATION_Y,
+                            mFab.getTranslationY(), mFab.getTranslationY() + mRadioImageGroup.getBottom() - mFab.getTop());
+                    AnimatorSet set = new AnimatorSet();
+                    set.play(animator).with(fabAnimator);
+                    set.setDuration(300);
+                    set.start();
+                } else if (dy <= 0 && !mIsBottomVisible) { // 可见
+                    mIsBottomVisible = true;
+                    ObjectAnimator animator = ObjectAnimator.ofFloat(mRadioImageGroup, View.TRANSLATION_Y,
+                            mRadioImageGroup.getTranslationY(), 0);
+                    ObjectAnimator fabAnimator = ObjectAnimator.ofFloat(mFab, View.TRANSLATION_Y,
+                            mFab.getTranslationY(), 0);
+                    AnimatorSet set = new AnimatorSet();
+                    set.play(animator).with(fabAnimator);
+                    set.setDuration(300);
+                    set.start();
+                }
+            }
+        });
     }
 
     private void loadData() {
